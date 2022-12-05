f6e75c9468838f653454640bd41e8554a8040fd0
shiv4j <119783592+shiv4j@users.noreply.github.com>
2022-12-05 11:35:00 +0530

    Update 9.c

--- 9.c
+++ 9.c
@@ -1,13 +1,4 @@
-#include <stdio.h> 
-int main() 
-{ 
-int var; /*Suppose address of var is 2000 */
-void *ptr = &var; 
-*(int*)ptr = 5; 
-printf("%d%d",var,*(int*)ptr); 			
-return 0; 
-}
-
+HI
 
 
 
